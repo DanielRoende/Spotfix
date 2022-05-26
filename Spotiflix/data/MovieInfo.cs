4e315d4a58a35570556c37f6fe210c4da9111fce
DHR <82029094+DanielRoende@users.noreply.github.com>
2022-05-26 13:05:11 +0200

    1Com
    Initial commit

---
+++ Spotiflix/data/MovieInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace Spotiflix.data
+{
+    public class MovieInfo
+    {           
+   
+            public string? overview { get; set; }
+            public string? poster_path { get; set; }
+            public string? title { get; set; }
+            public double vote_average { get; set; }       
+    }
+
+}
+
---
+++ Spotiflix/data/SiteDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace Spotiflix.data
+{
+    public class SiteDetails
+    {            
+            public List<MovieInfo> Results { get; set; }
+
+        
+    }
+
+}
+
